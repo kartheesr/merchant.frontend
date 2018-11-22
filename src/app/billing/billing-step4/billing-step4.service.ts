1a4a4e3ae8025992e1848c5eefb7387c6341eecf
madhan <madhanaraj.i@mitosistech.com>
2018-11-22 15:38:57 +0530

    Committed

--- src/app/billing/billing-step4/billing-step4.service.ts
+++ src/app/billing/billing-step4/billing-step4.service.ts
@@ -12,12 +12,14 @@ import { User } from '@app/models/User';
 export class BillingServiceCall {
   public actionUrl: string;
   public step4recuuringactionUrl: string;
+  public gasValueURL: string;
   private headers: HttpHeaders = new HttpHeaders({
     'Content-Type': 'application/json',
     'api-request-source': 'portal'
   });
   constructor(private _http: HttpClient) {
     this.actionUrl = `${Constants.apiPrefix}pull-payment-models/`;
+    this.gasValueURL = `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiO…TQzfQ.3imXDPiYRKPzWi9BQKhaFKeGVr-PtE5dgcjijQHZVHs`;
   }
   public billingPost(data): Observable<any> {
     return this._http.post(this.actionUrl, data, { headers: this.headers }).pipe(
@@ -29,5 +31,12 @@ export class BillingServiceCall {
   public Updateput(id, putdata): Observable<any> {
     console.log('API Hit PUT', id);
     return this._http.put(this.actionUrl + id, putdata, { headers: this.headers });
+  }
+  public gasvalueCalcualtion(): Observable<any> {
+    return this._http.get(this.gasValueURL).pipe(
+      map((response: HttpResponse) => {
+        return response;
+      })
+    );
   }
 }
